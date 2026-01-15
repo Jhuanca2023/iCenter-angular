c1883090915b84694b559b12580586861ed0b8a2
Jhuanca2023 <josehuanca612@gmail.com>
2026-01-14 20:58:04 -0500

    fix: corregir prerendering para rutas dinÃ¡micas y warnings de optional chaining

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,27 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'productos',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'productos/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'auth',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'auth/**',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Client
   }
 ];
